06bfa4422af67906caa2514459aa320d34b3e49b
roman <roman.rubinshtein@1win.pro>
2023-06-14 08:06:49 +0200

    added api prefix

--- bot-api/src/routes/mainRoute.js
+++ bot-api/src/routes/mainRoute.js
@@ -2,7 +2,9 @@ import Router from 'koa-router';
 import clientsRoute from '../app/clientsRoute';
 import usersRoute from '../app/usersRoute';
 
-const router = new Router();
+const router = new Router({
+    prefix: '/api',
+});
 
 router.use(clientsRoute);
 router.use(usersRoute);
