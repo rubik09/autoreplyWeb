a6055c9171f062970d4729ce2763db842cd30da8
roman <roman.rubinshtein@1win.pro>
2023-08-03 17:45:42 +0200

    deleted dev fucntion

--- bot-api/src/config.js
+++ bot-api/src/config.js
@@ -1,14 +1,16 @@
-const isProduction = process.NODE_ENV === 'production';
+// const isProduction = process.NODE_ENV === 'production';
 
-export const MYSQL_HOST = isProduction ? process.env.MYSQL_HOST_PROD : process.env.MYSQL_HOST_DEV;
-export const MYSQL_PORT = isProduction ? process.env.MYSQL_PORT_PROD : process.env.MYSQL_PORT_DEV;
-export const MYSQL_DATABASE = isProduction ? process.env.MYSQL_DATABASE_PROD : process.env.MYSQL_DATABASE_DEV;
-export const ADDRESS = isProduction ? process.env.ADDRESS_PROD : process.env.ADDRESS_DEV;
+// export const MYSQL_HOST = isProduction ? process.env.MYSQL_HOST_PROD : process.env.MYSQL_HOST_DEV;
+// export const MYSQL_PORT = isProduction ? process.env.MYSQL_PORT_PROD : process.env.MYSQL_PORT_DEV;
+// export const MYSQL_DATABASE = isProduction ? process.env.MYSQL_DATABASE_PROD : process.env.MYSQL_DATABASE_DEV;
+// export const ADDRESS = isProduction ? process.env.ADDRESS_PROD : process.env.ADDRESS_DEV;
+
+// console.log(MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE, ADDRESS)
 
 
 export const {
   MYSQL_USER, MYSQL_ROOT_PASSWORD,
-  SECRET_KEY,
+  SECRET_KEY,MYSQL_HOST, MYSQL_PORT,MYSQL_DATABASE, ADDRESS
 } = process.env;
 
 export const actionTypes = {
