4378a6f9496558b2cdda921fcb2cd909ea4d096a
roman <roman.rubinshtein@1win.pro>
2023-08-04 10:26:04 +0200

    chamges

--- bot-api/src/config.js
+++ bot-api/src/config.js
@@ -1,16 +1,7 @@
-// const isProduction = process.NODE_ENV === 'production';
-
-// export const MYSQL_HOST = isProduction ? process.env.MYSQL_HOST_PROD : process.env.MYSQL_HOST_DEV;
-// export const MYSQL_PORT = isProduction ? process.env.MYSQL_PORT_PROD : process.env.MYSQL_PORT_DEV;
-// export const MYSQL_DATABASE = isProduction ? process.env.MYSQL_DATABASE_PROD : process.env.MYSQL_DATABASE_DEV;
-// export const ADDRESS = isProduction ? process.env.ADDRESS_PROD : process.env.ADDRESS_DEV;
-
-// console.log(MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE, ADDRESS)
-
-
 export const {
-  MYSQL_USER, MYSQL_ROOT_PASSWORD,
-  SECRET_KEY,MYSQL_HOST, MYSQL_PORT,MYSQL_DATABASE
+  MYSQL_USER, MYSQL_DATABASE, MYSQL_ROOT_PASSWORD,
+  MYSQL_HOST, MYSQL_PORT,
+  SECRET_KEY,
 } = process.env;
 
 export const actionTypes = {
