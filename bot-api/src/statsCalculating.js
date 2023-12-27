1d12b0dfc076a70e20d3bbd9adf2202bd9086fcf
DenisLokhtin <denis.lokhtin@gmail.com>
2023-12-27 12:18:51 +0600

    users counting fix

--- bot-api/src/statsCalculating.js
+++ bot-api/src/statsCalculating.js
@@ -15,23 +15,21 @@ const statsCalculating = () => {
             if (!statsArr.length) await stats.addStats(0, api_id);
             const allUsers = await users.getCountUsers(api_id);
             const count = Object.values(allUsers[0])[0];
-            const countUsers = await stats.getCountStats(api_id);
-            const newUsersCount = count - countUsers[0].users_count;
             const mainStats = await stats.getStatsByApiId(api_id);
             const {incoming_messages_count} = mainStats[0];
             const keywords = await sessions.getKeywordsFromSession(api_id);
             const parsedKeywords = JSON.parse(keywords[0].keywords);
             const username = await sessions.getUsernameFromSession(api_id);
-            let averageMessagesCount = incoming_messages_count / newUsersCount;
-            if (incoming_messages_count < 1 || newUsersCount < 1) averageMessagesCount = 0;
+            let averageMessagesCount = incoming_messages_count / count;
+            if (incoming_messages_count < 1 || count < 1) averageMessagesCount = 0;
 
-            await statsSending(username[0].username, incoming_messages_count, newUsersCount, averageMessagesCount.toFixed(2), parsedKeywords);
+            await statsSending(username[0].username, incoming_messages_count, count, averageMessagesCount.toFixed(2), parsedKeywords);
 
             const newArr = zeroOutCounts(parsedKeywords);
             const stringifyNewArr = JSON.stringify(newArr);
 
             await sessions.updateKeywordsToSessionByApiId(stringifyNewArr, api_id);
-            await stats.updateClientStats(0, count[0], api_id);
+            await stats.updateClientStats(0, 0, api_id);
             await users.cleanTable();
         }
     }, {
