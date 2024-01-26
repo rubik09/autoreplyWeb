96f1823841b6c1e9e9ef190591c736999dd4dd4e
DenisLokhtin <denis.lokhtin@gmail.com>
2024-01-27 00:11:20 +0600

    hardcode baseUrl

--- bot-front/src/axiosApi.js
+++ bot-front/src/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosApi = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'http://207.154.241.72:8000',
 });
 
 export default axiosApi;
