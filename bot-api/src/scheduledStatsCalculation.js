109a595e5759aee50a0b524907ffeb60aba8c616
DenisLokhtin <denis.lokhtin@gmail.com>
2024-01-30 11:58:28 +0600

    fix time

--- bot-api/src/scheduledStatsCalculation.js
+++ bot-api/src/scheduledStatsCalculation.js
@@ -36,7 +36,7 @@ const calculating = async (time) => {
 };
 
 const scheduledStatsCalculation = () => {
-    cron.schedule('15 * * * * *', async () => {
+    cron.schedule('0 1 * * *', async () => {
         await calculating('10:00-00:00 - day');
     }, {scheduled: true, timezone: "Europe/Moscow"});
 
